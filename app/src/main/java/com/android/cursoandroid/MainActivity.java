0b9c99ac798ad0f5518f2d8f0d4bbca8ea1be158
alejandro <jaramil@gmail.com>
2015-10-21 20:45:19 -0500

    clase3-android

--- app/src/main/java/com/android/cursoandroid/MainActivity.java
+++ app/src/main/java/com/android/cursoandroid/MainActivity.java
@@ -1,13 +1,68 @@
 package com.android.cursoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txvEstado;
+    private String ciclo_vida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("CLASEANDORID", "ONCREATE");
+
+        txvEstado = (TextView)findViewById(R.id.tv_estado);
+
+        ciclo_vida = "Estado: onCreate";
+        txvEstado.setText(ciclo_vida);
+         }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("CLASEANDORID", "ONSTART");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("CLASEANDORID", "ONRESUME");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("CLASEANDORID", "ONPAUSE");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("CLASEANDORID", "ONSTOP");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("CLASEANDORID", "ONDESTROY");
+    }
+
+    public void iniciarActivity(View view){
+        Intent starter = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(starter);
+
+    }
+    public void finalizarActivity(View view){
+        finish();
     }
 }
+
